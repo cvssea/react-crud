75a26f1576d5d8c925645ec8b08d3e5b40dcb7a8
cvssea <42877793+cvssea@users.noreply.github.com>
2019-05-14 21:59:58 +0300

    Updates

---
+++ precache-manifest.fab91d0aa8485a6834675b389dc27e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbe252778b9ef90b2dcea91832c1f0d",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "83d35deff0d7464c61ee",
+    "url": "/react-crud/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "83d35deff0d7464c61ee",
+    "url": "/react-crud/static/js/2.673167cc.chunk.js"
+  },
+  {
+    "revision": "5558d0bca2528c0d5a1f",
+    "url": "/react-crud/static/js/main.a401b3f7.chunk.js"
+  },
+  {
+    "revision": "2f3d0c7cc7993cab9d36",
+    "url": "/react-crud/static/js/runtime~main.73d447a4.js"
+  }
+]);
